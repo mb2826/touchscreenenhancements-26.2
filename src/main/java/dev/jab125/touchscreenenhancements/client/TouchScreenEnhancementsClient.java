9ab4478d680ca256c3ade73e75c1a67158111118
mb2826 <mb2826@edu.turku.fi>
2026-09-17 11:36:52 +0300

    Add client initializer

---
+++ src/main/java/dev/jab125/touchscreenenhancements/client/TouchScreenEnhancementsClient.java
@@ -0,0 +1,10 @@
+package dev.jab125.touchscreenenhancements.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TouchScreenEnhancementsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client init - mixins handle the rest
+    }
+}
